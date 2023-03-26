e2599eb6921916d5af06361dadc97846310c0848
Adrian Rutkowski <alurendium@gmail.com>
2023-03-26 23:08:23 +0800

    add environments validation and remove enum type

--- Runtime/Scripts/Core/Data/EnvironmentData/EnvironmentsDataDefinition.cs
+++ Runtime/Scripts/Core/Data/EnvironmentData/EnvironmentsDataDefinition.cs
@@ -5,8 +5,8 @@ namespace JoystickRemoteConfig.Core.Data
     public class EnvironmentsDataDefinition : ScriptableObject
     {
         public EnvironmentData[] environments;
-        [HideInInspector] public EnvironmentType environmentType;
-        
+        [HideInInspector] public EnvironmentType environmentType; //ToDo Remove Giver
+        [HideInInspector] public EnvironmentData selectedEnvironment = new();
         private bool _updateEnvironment;
     }
 }
